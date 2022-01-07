38bd09aaebd19ce6479c1463b74237e53df55e7e
Tapio Salonen <tsa.dom@pm.me>
2022-01-07 22:50:04 +0200

    Modified build scripts

---
+++ generate.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const { marked } = require('marked')
+
+try {
+  let data = fs.readFileSync(process.argv[2], 'utf8')
+  const rx = new RegExp("<!---[\\d\\D]*?\--->")
+  while (data.includes('<!---')) data = data.replace(rx, '')
+  const html = marked.parse(data)
+  console.log(html)
+} catch (err) {
+  console.log(err)
+}
